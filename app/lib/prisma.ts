3f3cb122ab9b3005c57f068df993e6075fadc9aa
larestrepo <larestrepomu@gmail.com>
2025-01-31 15:31:48 -0500

    first commint to new vercel

--- app/lib/prisma.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-// ✅ Ensure Prisma is initialized properly
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({log: ['query', 'info', 'warn', 'error'],});
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
